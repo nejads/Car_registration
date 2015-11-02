691cdf255b269e4f6815755597e36c268e98ac20
Soroush <sardabir@gmail.com>
2015-11-02 11:06:28 +0100

    bug fixed

--- carreg/urls.py
+++ carreg/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from carreg_app import index
+from carreg_app.views import index
 
 urlpatterns = [
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- carreg_app/views.py
+++ carreg_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('You do NOT need gasstation any longer!')
+    html = "<html><body>You do NOT need gas station any longer!</body></html>"
+    return HttpResponse(html)
