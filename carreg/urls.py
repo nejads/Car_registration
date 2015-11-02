e92d3cb8733215b6b0516880e0e9c5d89693bc5f
Soroush <sardabir@gmail.com>
2015-11-02 10:58:10 +0100

    Index page added

--- carreg/urls.py
+++ carreg/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from carreg_app import index
 
 urlpatterns = [
+    url(r'^$', index, name='index')
     url(r'^admin/', include(admin.site.urls)),
 ]
--- carreg_app/views.py
+++ carreg_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('You do NOT need gasstation any longer!')
