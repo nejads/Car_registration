460002a15c04c35b475c33c32e385af3337585d7
Soroush <nejads@student.chalmers.se>
2015-11-04 23:02:05 +0100

    setting has updated to test the app

--- carreg/settings.py
+++ carreg/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'carreg_app',
     'rest_framework',
+    'rest_framework_swagger',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,13 +103,6 @@ else:
         },
     }
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
