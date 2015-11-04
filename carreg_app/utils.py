7c5cc77ec8f1ece60de46e292949322351de0c68
Soroush <nejads@student.chalmers.se>
2015-11-04 23:03:53 +0100

    added to_json functionality to utils

--- carreg_app/utils.py
+++ carreg_app/utils.py
@@ -1,4 +1,20 @@
+import os
+from base64 import b64encode
+
+from rest_framework.renderers import JSONRenderer
+
 
 def validate_phone():
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Not allowed telephone number")
+
+
+def salt_gen():
+    random_bytes = os.urandom(32)
+    random_string = b64encode(random_bytes).decode('utf-8')
+    return random_string
+
+
+def to_json(dictionary):
+    json = JSONRenderer().render(dictionary)
+    return json
